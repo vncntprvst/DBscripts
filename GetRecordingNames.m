7514905887f354bd9f94efed0f7ea0925021f691
Vincent <vincent@server.com>
2018-07-22 23:56:27 -0400

    update to get recording names

--- GetRecordingNames.m
+++ GetRecordingNames.m
@@ -1,12 +1,13 @@
+
 % get recording names from database
 
-unitList=unitsDBinfo.rec_id;  %cellfun(@(x) x.unit_id, unitsDBinfo); Changed to a table
+unitList=unitsDBinfo.rec_id;  %cellfun(@(x) x.unit_id, unitsDBinfo); Changed to a table (see CellArrayOfStruct2Table)
 recNames=fetch(dbConn,['SELECT a_file FROM recordings r WHERE r.recording_id IN (' ...
     sprintf('%.0f,' ,unitList(1:end-1)) num2str(unitList(end)) ')']);
 % just to make sure, re-order 
 [~,resort]=sort(unitList);[~,resort]=sort(resort);recNames=recNames(resort);
 
 % add names to data structure
-data.gsdata.alldb=[data.gsdata.alldb cell2table(recNames)];
+data.cmdata.alldb=[data.cmdata.alldb cell2table(recNames)];
 
 
