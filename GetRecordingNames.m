69ba7cbd2e3ed443149977bc9fcd061c09227cda
Vincent <vincent@server.com>
2019-06-30 20:09:58 -0400

    minor edit

--- GetRecordingNames.m
+++ GetRecordingNames.m
@@ -1,6 +1,7 @@
 
 % get recording names from database
-
+dbConn = connect2DB('vp_sldata'); 
+unitsDBinfo=stdata.alldb;
 unitList=unitsDBinfo.rec_id;  %cellfun(@(x) x.unit_id, unitsDBinfo); Changed to a table (see CellArrayOfStruct2Table)
 recNames=fetch(dbConn,['SELECT a_file FROM recordings r WHERE r.recording_id IN (' ...
     sprintf('%.0f,' ,unitList(1:end-1)) num2str(unitList(end)) ')']);
@@ -9,5 +10,5 @@ recNames=fetch(dbConn,['SELECT a_file FROM recordings r WHERE r.recording_id IN 
 
 % add names to data structure
 data.cmdata.alldb=[data.cmdata.alldb cell2table(recNames)];
-
+stdata.fileNames=stdata.alldb.recNames;
 
